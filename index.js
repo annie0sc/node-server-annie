e40e09d4a28df93d77fc36b40e1b29b01d29d51e
annie0sc <annie0sc@gmail.com>
2020-03-24 23:35:40 -0500

    undid all previous changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require('http')
+const http = require('http');
 
-const hostname = '127.0.0.1'
+const hostname = process.env.hostname || '0.0.0.0' || '127.0.0.1';
 
-const PORT = 3000
+const port = process.env.PORT || 3000;
 
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
+    res.statusCode = 200
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hi, Welcome to the wonderful world of Annie Samarpitha Chandolu!');
 });
