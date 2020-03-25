778ee7df646ccd0c73a5b9b2eb971e934ec8a98b
annie0sc <annie0sc@gmail.com>
2020-03-24 21:30:10 -0500

    trying to resolve error R10

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = process.env.hostname || '127.0.0.1';
+const hostname = process.env.hostname || '0.0.0.0';
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
