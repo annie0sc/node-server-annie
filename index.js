a4bed9efabaaa3afc78bb4639d7ae34fb63cf2f8
annie0sc <annie0sc@gmail.com>
2020-03-24 23:57:10 -0500

    changed the port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = process.env.hostname || '0.0.0.0' || '127.0.0.1';
+const hostname = process.env.hostname || '127.0.0.1';
 
 const port = process.env.PORT || 3000;
 
