00b1e9bc27426f901b5595288323228c41747c2e
annie0sc <annie0sc@gmail.com>
2020-03-24 21:07:43 -0500

    Changed port

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const http = require('http')
+const http = require('http');
 
-const hostname = process.env.hostname || '127.0.0.1'
-const port = process.env.PORT || 3000
+const hostname = process.env.hostname || '127.0.0.1';
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hi, Welcome to the wonderful world of Annie Samarpitha Chandolu!')
-})
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hi, Welcome to the wonderful world of Annie Samarpitha Chandolu!');
+});
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-})
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
